408e0ae922d3de23d4346ff578f4c030509d6d25
Michel Mariano <72361556+MichelMariano@users.noreply.github.com>
2024-04-01 20:25:40 -0300

    Update calculando-descontos.py

--- calculando-descontos.py
+++ calculando-descontos.py
@@ -5,3 +5,19 @@ preço = float(input('Qual é o Preço do Produto? R$ '))
 novo = preço - (preço * 5 / 100)
 
 print('O produto teve um desconto de 5% e se novo valor é R$ {:.2f} '.format(novo))
+
+
+
+-----------------------------------
+
+# desconto a vista e acrescimo a prazo
+
+prod = input('Digite o Produto: ')
+preço = float(input('Qual é o Preço do Produto? R$ '))
+novo = preço - (preço * 5 / 100)
+prazo = preço + (preço * 10/100)
+print('=' * 90)
+print('O produto {} tem de 5% de desconto para pagamento a vista e se novo valor é R$ {:.2f} '.format(prod, novo))
+print('-' * 90)
+print('O produto {} pagando a prazo, terá um acrescimento de 10% e passara a valer R$ {} '.format(prod, prazo))
+print('=' * 90)
